eadfed0373486cbc1081f389d7a3fdaa3de8b1b1
PLecicki <p.lecicki@gmail.com>
2023-09-28 10:18:15 +0200

    New project

---
+++ src/main/java/pl/piotr/lecicki/phishingvalidatorplay/PhishingValidatorPlayApplication.java
@@ -0,0 +1,13 @@
+package pl.piotr.lecicki.phishingvalidatorplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingValidatorPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhishingValidatorPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/piotr/lecicki/phishingvalidatorplay/PhishingValidatorPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piotr.lecicki.phishingvalidatorplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingValidatorPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
